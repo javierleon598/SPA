5bcce8fd4fe03a328b5e2cdbabcf6ca7ce8fb780
JAVIER ALEXANDER HERNANDEZ LEON <hernandezjavl@HILDEBOG.LOCAL>
2020-11-06 22:23:21 -0500

    Router 2 opc

--- singlePageApp/src/app/app.module.ts
+++ singlePageApp/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HomeComponent } from './components/home/home.component';
     NavbarComponent,
     AboutComponent,
     HeroesComponent,
-    HomeComponent
+    HomeComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
--- singlePageApp/src/app/components/heroes/heroes.component.ts
+++ singlePageApp/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe, HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +11,15 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[];
 
-  constructor( private _heroesService: HeroesService) { }
+  constructor( private _heroesService: HeroesService, private router: Router) { }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe(i:number){
+    this.router.navigate(['/heroe',i]);
   }
 
 }
