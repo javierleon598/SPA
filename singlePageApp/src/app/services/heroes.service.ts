8628dc2984fc1bfffa64287c3b60aed3aac24748
JAVIER ALEXANDER HERNANDEZ LEON <hernandezjavl@HILDEBOG.LOCAL>
2020-11-06 22:01:45 -0500

    Primer Interface

--- singlePageApp/src/app/services/heroes.service.ts
+++ singlePageApp/src/app/services/heroes.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 
+export interface Heroe {
+  nombre: string,
+  bio: string,
+  img: string,
+  aparicion: string,
+  casa:string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroesService {
 
-  private heroes: any[] = [
+  private heroes: Heroe[] = [
     {
       nombre: "Aquaman",
       bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
@@ -62,7 +70,7 @@ export class HeroesService {
     console.log("Servicio Heroes!!!");
   }
 
-  getHeroes(){ 
+  getHeroes(): Heroe[] { 
     return this.heroes;
   }
 
